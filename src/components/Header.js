e53bbe2b00c19708ed76c30b3e865809ee7ff17e
Philippe Roemer <philipperoemer@yahoo.com>
2022-02-01 13:20:55 -0500

    Date and header container added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,32 @@ import React, { useState, useEffect } from "react";
 
 const Header = () => {
     const [currentTime, setCurrentTime] = useState([]);
+    const [currentDate, setCurrentDate] = useState([]);
 
     const Time = () => {
         const time = new Date();
         setCurrentTime(time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", hour12: true }));
     };
 
+    const CalendarDate = () => {
+        const date = new Date();
+        setCurrentDate(date.toLocaleString("en-US", { month: "long", day: "2-digit", year: "numeric" }));
+    };
+
     useEffect(() => {
         Time();
+        CalendarDate();
         const interval = setInterval(() => {
             Time();
+            CalendarDate();
         }, 1000);
         return () => clearInterval(interval);
     }, []);
 
     return (
-        <div>
+        <div className="headerContainer">
             <h3>{currentTime}</h3>
+            <h3>{currentDate}</h3>
         </div>
     );
 };
