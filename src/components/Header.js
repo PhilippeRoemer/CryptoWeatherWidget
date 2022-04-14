98a7d91cfbd4153c9c789c68c69cba7d76d8c8e4
Philippe Roemer <philipperoemer@yahoo.com>
2022-04-14 09:08:12 -0400

    Austronaut information container removed due to unreliable API sources

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,6 @@ import SpaceRocket from "../Rocket.png";
 const Header = () => {
     const [currentTime, setCurrentTime] = useState([]);
     const [currentDate, setCurrentDate] = useState([]);
-    /*     const [currentAstronauts, setCurrentAstronauts] = useState([]);
-    const [astronautInfo, setAstronautInfo] = useState([]); */
 
     const Time = () => {
         const time = new Date();
@@ -18,17 +16,7 @@ const Header = () => {
         setCurrentDate(date.toLocaleString("en-US", { month: "long", day: "2-digit", year: "numeric" }));
     };
 
-    /*     const AustronautData = () => {
-        axios
-            .get("http://api.open-notify.org/astros.json")
-            .then((res) => {
-                setCurrentAstronauts(res.data.number);
-                setAstronautInfo(res.data.people);
-            })
-            .catch((error) => console.log(error));
-    }; */
-
-    /* Date and time refreshed every 1 second */
+    /* Date and time refreshed every second */
     useEffect(() => {
         Time();
         CalendarDate();
@@ -39,15 +27,6 @@ const Header = () => {
         return () => clearInterval(interval);
     }, []);
 
-    /* Austronaut data is refreshed every 30 minutes*/
-    /*     useEffect(() => {
-        AustronautData();
-        const interval = setInterval(() => {
-            AustronautData();
-        }, 1800000);
-        return () => clearInterval(interval);
-    }, []); */
-
     return (
         <div className="headerContainer">
             <div>
@@ -57,31 +36,6 @@ const Header = () => {
             <div>
                 <h3 className="date">{currentDate}</h3>
             </div>
-
-            {/*             <div className="spaceContainer ">
-                <div className="rocketContainer">
-                    <h3>{currentAstronauts}</h3>
-                    <img src={SpaceRocket} className="rocketIcon" />
-                </div>
-                <p className="spaceText">People in space</p>
-                <div className="tooltiptext">
-                    {astronautInfo.map((post) => {
-                        const craft = post.craft;
-                        const craftSearch = "http://www.google.com/search?q=" + post.craft;
-                        const name = post.name;
-                        return (
-                            <div>
-                                <p>
-                                    <a href={craftSearch} target="_blank">
-                                        {craft}
-                                    </a>
-                                    : {name}
-                                </p>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div> */}
         </div>
     );
 };
